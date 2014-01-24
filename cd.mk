34b9ca07de3fda2fc3f70d6bc8dac2371e2ae1f1
yanniks <kontakt@yanniks.de>
2014-01-24 10:11:41 +0100

    initial CyanDream commit

--- cd.mk
+++ cd.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/oppo/n1/full_n1.mk)
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_n1
+PRODUCT_NAME := cd_n1
 PRODUCT_DEVICE := n1
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
